3b9e1cb4f61ca3e0b6cccf27a16c7b52809abd3f
Nathan BENHADOUCHE <nbenhado@e3r13p4.42.fr>
2021-12-08 15:51:30 +0100

    stdarg

--- ft_printf.c
+++ ft_printf.c
@@ -6,18 +6,45 @@
 /*   By: nbenhado <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/07 14:36:47 by nbenhado          #+#    #+#             */
-/*   Updated: 2021/12/07 14:39:12 by nbenhado         ###   ########.fr       */
+/*   Updated: 2021/12/08 15:49:27 by nbenhado         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
+#include "libft/libft.h"
 
+//cspdiuxX%
 
-int ft_printf(const char *, ...)
+void	ft_putchar(char c)
 {
-   va_list va;
-   va_start(va, s);
-			
+	write(1, &c, 1);
+}
+char evaluate_format(char c)
+{
+	if (c == 'c')
+		return 		
+}
+
+int ft_printf(const char *str, ...)
+{
+	int i;
+	int ret;
+	va_list arg_ptr;
+	
+	ret = 0;
+	va_start(arg_ptr, str);
+	while(str[i])
+   {
+	   if (str[i] == '%')
+	   {
+			Evaluate_format(str[i + 1]);
+			va_arg(arg_ptr, le type);
+	   }
+	   ft_putchar(str[i]);
+	   i++;
+   }
+	va_end(arg_ptr);
+	return (ret);		
 }
 
 // Une fonction qui initialise un les valeurs du tableau
@@ -26,3 +53,5 @@ int ft_printf(const char *, ...)
 // 			des que string[i] == %, cest qu'on a a faire a une variable
 // 			Evaluer le format de la variable rencontrer
 // Une fonction EVALUATE_FORMAT
+
+	
